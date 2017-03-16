b2327a0a121b96f3b93fa3d64868458be5495fbd
user <user@users-MacBook-Pro-147.local>
2017-03-16 23:41:14 +0000

    fixed syntax sql

--- src/main.py
+++ src/main.py
@@ -28,30 +28,34 @@ class webcrawler:
         APIKEY = 'a360b2a061d254a3a5891e4415511251899f6df1'
         NAME = "Dublin"
         STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
+        
         r = requests.get(STATIONS_URI, params={"apiKey": APIKEY,
-                                                           "contract": NAME})
+                                                                   "contract": NAME})
         data = (json.loads(r.text))
-        for row in data:
-            name = row['name']
-            status = row['status']
-            bike_stands = row['bike_stands']
-            available_bike_stands = row['available_bike_stands']
-            available_bikes = row['available_bikes']
-            last_update = row['last_update']
+    
                     
-            starttime=time.time()
-            engine = create_engine("mysql+pymysql://Project1Team13:Renault4@project1team13.cldi9otgx37k.us-west-2.rds.amazonaws.com:3306/Project1Team13")
-            connection = engine.connect()
-           
-            while starttime<1489680980800:
-                
-                try:
-                    result_insert = connection.execute('INSERT INTO DublinBikes (name,status,bike_stands,available_bike_stands,available_bikes,last_update) VALUES (name,status,bike_stands,available_bike_stands,available_bikes,last_update)')
-                    time.sleep(5*60)
-                except:
-                    print (traceback.format_exc())
+        starttime=time.time()
+        engine = create_engine("mysql+pymysql://Project1Team13:Renault4@project1team13.cldi9otgx37k.us-west-2.rds.amazonaws.com:3306/Project1Team13")
+        connection = engine.connect()
+       
+        while starttime<1489680980800:
             
-            return
+            try:
+                for row in data:
+                    number= row['number']
+                    name = row['name']
+                    status = row['status']
+                    bike_stands = row['bike_stands']
+                    available_bike_stands = row['available_bike_stands']
+                    available_bikes = row['available_bikes']
+                    banking = row['banking']
+                    last_update = row['last_update']
+                    connection.execute('INSERT INTO DublinBikes (number, name, status, bike_stands, available_bike_stands,available_bikes, banking, last_update) VALUES (%s,%s, %s, %s,%s, %s, %s, %s)', (number, name, status, bike_stands, available_bike_stands, available_bikes, banking, last_update))
+                time.sleep(5*60)
+            except:
+                print (traceback.format_exc())
+        
+        return
 
     #http://pythondata.com/collecting-storing-tweets-python-mysql/ 
     
