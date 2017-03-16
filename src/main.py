b5076033b324510521c91c59a2a28d0bdd1d6291
user <user@users-MacBook-Pro-147.local>
2017-03-16 15:21:17 +0000

    variables syntax corrected

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def main():
     parser.add_argument('--input', help='no inputs needed to run code')
     args=parser.parse_args()
     tester=webcrawler()
-    tester.create_table(tester.connection)
+    #tester.create_table(tester.connection)
     tester.store_data(tester.r,tester.data,tester.engine, tester.connection)
     
     
@@ -35,31 +35,31 @@ class webcrawler:
     engine = create_engine("mysql+pymysql://Project1Team13:Renault4@project1team13.cldi9otgx37k.us-west-2.rds.amazonaws.com:3306/Project1Team13")
     connection = engine.connect() 
 
-    def create_table(self, connection):
-        result_create = connection.execute("CREATE TABLE IF NOT EXISTS DublinBikes(number int NOT NULL,name varchar(255),address varchar(255),position varchar(255),banking varchar(255),bonus varchar(255),status varchar(255),contract_name varchar(255),bike_stands int,available_bike_stands int,available_bikes int,last_update datetime);") 
+    #def create_table(self, connection):
+        #result_create = connection.execute("CREATE TABLE IF NOT EXISTS DublinBikes(number int NOT NULL,name varchar(255),address varchar(255),position varchar(255),banking varchar(255),bonus varchar(255),status varchar(255),contract_name varchar(255),bike_stands int,available_bike_stands int,available_bikes int,last_update datetime);") 
         
     def store_data(self, r, data, engine, connection):
-        number = data[0]
-        name = data[1]
-        address = data[2]
-        position = data[3]
-        banking = data[4]
-        bonus = data[5]
-        status = data[6]
-        contract_name = data[7]
-        bike_stands = data[8]
-        available_bike_stands = data[9]
-        available_bikes = data[10]
-        last_update = data[11]
-        starttime=time.time()
-        while True:
-            try:
-                result_insert = connection.execute('INSERT INTO DublinBikes (number,name,address,position,banking,bonus,status,contract_name,bike_stands,available_bike_stands,available_bikes,last_update) VALUES (number,name,address,position,banking,bonus,status,contract_name,bike_stands,available_bike_stands,available_bikes,last_update)')
-                time.sleep(5*60)
-                connection.close()
-            except:
-                print (traceback.format_exc())
-        return
+        for row in data:
+            number = row['number']
+            name = row['name']
+            address = row['address']
+            position = row['position']
+            banking = row['banking']
+            bonus = row['bonus']
+            status = row['status']
+            contract_name = row['contract_name']
+            bike_stands = row['bike_stands']
+            available_bike_stands = row['available_bike_stands']
+            available_bikes = row['available_bikes']
+            last_update = row['last_update']
+            starttime=time.time()
+            while True:
+                try:
+                    result_insert = connection.execute('INSERT INTO DublinBikes (number,name,address,position,banking,bonus,status,contract_name,bike_stands,available_bike_stands,available_bikes,last_update) VALUES (number,name,address,position,banking,bonus,status,contract_name,bike_stands,available_bike_stands,available_bikes,last_update)')
+                    time.sleep(5*60)
+                except:
+                    print (traceback.format_exc())
+            return
 
     #http://pythondata.com/collecting-storing-tweets-python-mysql/ 
     
